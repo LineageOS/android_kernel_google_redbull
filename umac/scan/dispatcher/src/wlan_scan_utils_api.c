049759cf3ea8ec9cd0c506a59e068a042732b221
Hsiu-Chang Chen <hsiuchangchen@google.com>
2024-02-19 14:02:19 +0800

    qcacmn: handle integer underflow in util_gen_new_ie
    Handle integer underflow for subie_len in util_gen_new_ie
    
    Bug: 318386769
    Test: Regression Test
    Change-Id: I2f73e5a7e0462100deae1e85e6a51f77bfc46b95
    CRs-Fixed: 3582487
    Signed-off-by: Hsiu-Chang Chen <hsiuchangchen@google.com>

--- umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -1419,7 +1419,9 @@ static uint32_t util_gen_new_ie(uint8_t *ie, uint32_t ielen,
 	 * copied to new ie, skip ssid, capability, bssid-index ie
 	 */
 	tmp_new = sub_copy;
-	while (tmp_new + tmp_new[1] + 2 - sub_copy <= subie_len) {
+	while ((subie_len > 0) &&
+	       (((tmp_new + tmp_new[1] + 2) - sub_copy) <=
+		(subie_len - 1))) {
 		if (!(tmp_new[0] == WLAN_ELEMID_NONTX_BSSID_CAP ||
 		      tmp_new[0] == WLAN_ELEMID_SSID ||
 		      tmp_new[0] == WLAN_ELEMID_MULTI_BSSID_IDX ||
